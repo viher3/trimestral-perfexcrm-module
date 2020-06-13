2b70b495a6c077084693219c69005c99f3c26b9c
viher3 <viher_3@hotmail.com>
2020-06-13 17:33:21 +0200

    Spanish language

---
+++ language/spanish/trimestral_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['title'] = 'Descargar gastos e ingresos';
+$lang['description'] = 'Selecciona un rango de fechas para descargar todas las facturas y gastos registrados organizados por años y meses.';
+$lang['init_date'] = 'Fecha inicio';
+$lang['end_date'] = 'Fecha fin';
+$lang['download'] = 'Descargar archivos';
--- views/index.php
+++ views/index.php
@@ -8,28 +8,28 @@
                 <div class="panel_s">
                     <div class="panel-body">
                         <div>
-                            <h4 style="margin:0">Descargar gastos e ingresos</h4>
+                            <h4 style="margin:0"><?= _l('title') ?></h4>
                         </div>
                         <div class="clearfix"></div>
                         <hr class="hr-panel-heading" />
 
-                        <p>Selecciona un rango de fechas para descargar todas las facturas y gastos registrados organizados por años y meses.</p>
+                        <p><?= _l('description') ?></p>
                         <br />
 
                         <div class="input-group">
-                            <label for="init_date">Fecha de inicio</label>
+                            <label for="init_date"><?= _l('init_date') ?></label>
                             <input name="init_date" id="init_date" type="date" class="form-control input-sm" />
                         </div>
                         <br />
                         <div class="input-group">
-                            <label for="end_date">Fecha de fin</label>
+                            <label for="end_date"><?= _l('end_date') ?></label>
                             <input name="end_date" id="end_date" type="date" class="form-control input-sm" />
                         </div>
 
                         <br />
                         <p align="center">
                             <a class="btn btn-info pull-left display-block mright5" href="<?= admin_url('trimestral/download') ?>">
-                                <i class="fa fa-download"></i>&nbsp;&nbsp;Descargar archivos
+                                <i class="fa fa-download"></i>&nbsp;&nbsp;<?= _l('download') ?>
                             </a>
                         </p>
 
@@ -39,7 +39,5 @@
         </div>
     </div>
 </div>
-
-
 
 <?php init_tail(); ?>
