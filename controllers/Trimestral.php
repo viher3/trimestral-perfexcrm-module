b648722a5e50ef1653b4cd3a70109fd594d54750
viher3 <viher_3@hotmail.com>
2020-06-13 23:40:50 +0200

    Force download

--- controllers/Trimestral.php
+++ controllers/Trimestral.php
@@ -4,17 +4,24 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 require(FCPATH . 'modules/trimestral/services/DownloadService.php');
 
+/**
+ * Class Trimestral
+ */
 class Trimestral extends AdminController
 {
     const EXPORT_PATH = FCPATH . 'temp/modules/trimestral/';
 
     const UPLOADS_PATH = FCPATH . 'uploads';
 
+    /**
+     * Trimestral constructor.
+     */
     public function __construct()
     {
         parent::__construct();
         $this->load->model('trimestral_model');
         $this->load->model('invoices_model');
+        $this->load->helper('download');
     }
 
     public function index()
@@ -22,19 +29,22 @@ class Trimestral extends AdminController
         $this->load->view('index');
     }
 
+    /**
+     * @throws Exception
+     */
     public function download()
     {
-        $downloadService = new DownloadService(
-            $this->trimestral_model,
-            $this->invoices_model,
-            $this->input->post('init_date'),
-            $this->input->post('end_date')
-        );
-        $downloadService->execute();
-
-        // TODO: compress folder
-        die('--fin--');
+        try{
+            $downloadService = new DownloadService(
+                $this->trimestral_model,
+                $this->invoices_model,
+                $this->input->post('init_date'),
+                $this->input->post('end_date')
+            );
+            $downloadService->execute();
+            $downloadService->forceDownload($downloadService->fileId());
+        }catch(Exception $e){
+            throw $e;
+        }
     }
-
-
 }
--- services/DownloadService.php
+++ services/DownloadService.php
@@ -18,6 +18,10 @@ class DownloadService
 
     private $invoices_model;
 
+    private $fileId;
+
+    private $fullFilePath;
+
     /**
      * DownloadService constructor.
      * @param $trimestralModel
@@ -31,6 +35,8 @@ class DownloadService
         $this->invoices_model = $invoicesModel;
         $this->initDate = $initDate;
         $this->endDate = $endDate;
+        $this->fileId = null;
+        $this->fullFilePath = null;
     }
 
     /**
@@ -38,41 +44,83 @@ class DownloadService
      */
     public function execute()
     {
-        try{
+        try {
             // get data
             $expenses = $this->trimestral_model->getExpenses();
             $invoices = $this->trimestral_model->getInvoices();
 
             // Init folder
             $id = (new DateTime())->format('Ymdhis');
-            $pathId = Trimestral::EXPORT_PATH . 'exports/' . $id;
+            $pathId = $this->generatePathId($id);
+            $this->fileId = $id;
+            $this->fullFilePath = $pathId . '.zip';
             mkdir($pathId, 0777, true);
 
             $expenseProcessor = new ExpenseProcessor();
 
             // Expenses
-            foreach($expenses as $expense)
-            {
+            foreach ($expenses as $expense) {
                 $expenseProcessor->execute($expense['date'], $pathId, 'expenses', $expense['id']);
             }
 
             $invoicesProcessor = new InvoiceProcessor($this->invoices_model);
 
             // Invoices
-            foreach($invoices as $invoice)
-            {
+            foreach ($invoices as $invoice) {
                 $invoicesProcessor->execute($invoice, $pathId);
             }
 
             // Zip folder
+            $zipFilename = $id . '.zip';
             $zipper = new FolderZipper($pathId);
-            $zipper->compress($id . '.zip');
+            $zipper->compress($zipFilename);
 
             // Remove temp folder
             FolderDeletion::remove($pathId);
-
-        }catch(Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
+    }
+
+    /**
+     * @param $fileId
+     * @throws Exception
+     */
+    public function forceDownload($fileId)
+    {
+        $filepath = $this->generatePathId($fileId);
+        $filepath = str_replace('exports/', '', $filepath) . '.zip';
+
+        if(!file_exists($filepath)){
+            throw new Exception(sprintf('File "%s" does not exists.', $filepath));
+        }
+
+        $fileContent = file_get_contents($filepath);
+        force_download('trimestral_export.zip', $fileContent, 'application/zip');
+    }
+
+    /**
+     * @return null
+     */
+    public function fullFilePath()
+    {
+        return $this->fullFilePath;
+    }
+
+    /**
+     * @return null
+     */
+    public function fileId()
+    {
+        return $this->fileId;
+    }
+
+    /**
+     * @param $id
+     * @return string
+     */
+    public function generatePathId($id)
+    {
+        return Trimestral::EXPORT_PATH . 'exports/' . $id;
     }
 }
